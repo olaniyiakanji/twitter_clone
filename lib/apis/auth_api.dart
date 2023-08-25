039c6366371f3bea7bbff0f41539ed9375c2a5ec
Umar Niyilola <niyilola@pop-os.localdomain>
2023-08-25 07:33:25 +0100

    Initial Commit

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,9 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract class IAuthAPI {
+  Future<Either<Account, String>> signUp({
+    required String email,
+    required String password,
+  });
+}
---
+++ lib/core/type_defs.dart
@@ -0,0 +1,4 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/core/core.dart';
+
+typedef FutureEither<T> = Future<Either<Failure, T>>;

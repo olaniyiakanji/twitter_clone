b6dfc8c5c1f43d14fd5a04e3b30d5aab5bfd086f
Umar Niyilola <niyilola@pop-os.localdomain>
2023-08-28 22:32:30 +0100

    error commit

--- lib/core/type_defs.dart
+++ lib/core/type_defs.dart
@@ -2,3 +2,4 @@ import 'package:fpdart/fpdart.dart';
 import 'package:twitter_clone/core/core.dart';
 
 typedef FutureEither<T> = Future<Either<Failure, T>>;
+typedef FutureEithervoid = FutureEither<void>;

29b00c83b4d23d051d13e2e07b3538fba1091d47
Umar Niyilola <niyilola@pop-os.localdomain>
2023-08-28 22:31:00 +0100

    error commit

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,9 +1,26 @@
 import 'package:appwrite/appwrite.dart';
-import 'package:fpdart/fpdart.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:twitter_clone/core/core.dart';
+
+
+// when you want to signup or get user account you use -> Acoount
+// when you want to get user data, you use Account from model, i.e model.Account i.e a model is returned
 
 abstract class IAuthAPI {
-  Future<Either<Account, String>> signUp({
+  FutureEither<model.Account> signUp({
     required String email,
     required String password,
   });
+}
+
+class AuthAPI implements IAuthAPI{
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
+  @override
+  FutureEither<dynamic> signUp({required String email, required String password}) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  } async{
+
+  }
 }

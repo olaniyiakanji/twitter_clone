e2f5412b1624204371e5256cbc3adbb628ba7166
Umar Niyilola <niyilola@pop-os.localdomain>
2023-09-08 13:15:18 +0100

    here is our new commit

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,26 +1,34 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart' as model;
+import 'package:fpdart/fpdart.dart';
 import 'package:twitter_clone/core/core.dart';
 
-
-// when you want to signup or get user account you use -> Acoount
-// when you want to get user data, you use Account from model, i.e model.Account i.e a model is returned
+// when you want to signup or get user account you use -> Acoount it is ralated to creating and mnaaging data in the server side
+// when you want to get user data, you use Account from model, i.e model.User  i.e a model is returned, user related date
 
 abstract class IAuthAPI {
-  FutureEither<model.Account> signUp({
+  FutureEither<model.User> signUp({
     required String email,
     required String password,
   });
 }
 
-class AuthAPI implements IAuthAPI{
+class AuthAPI implements IAuthAPI {
   final Account _account;
   AuthAPI({required Account account}) : _account = account;
   @override
-  FutureEither<dynamic> signUp({required String email, required String password}) {
+  FutureEither<model.User> signUp(
+      {required String email, required String password}) async {
     // TODO: implement signUp
-    throw UnimplementedError();
-  } async{
-
+    try {
+      final account = await _account.create(
+          userId: 'unique()', email: email, password: password);
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+          Failure(e.message ?? 'some unexpected error occured', stackTrace));
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
   }
 }

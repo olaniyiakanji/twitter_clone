6f8397819b0a8271fe851bb4443c2c3a55875c76
Umar Niyilola <niyilola@pop-os.localdomain>
2023-09-22 00:38:47 +0100

    working version but there is still issues with tweet

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,12 +1,12 @@
 class AppwriteConstants {
-  static const String databaseId = '650a24bdcecf09c2a653';
-  static const String projectId = '650a2484b1bf1ecf786b';
-  static const String endPoint = 'https://192.168.77.143:443/v1';
+  static const String databaseId = '650c678c7ac6ac69ba40';
+  static const String projectId = '650c65112b86c18555eb';
+  static const String endPoint = 'https://172.20.10.5:443/v1';
 
 
-  static const String usersCollection = '650a25772ae2370720d6';
-  static const String tweetsCollection = '650a2582be76d68e8f22';
-  static const String notificationsCollection = '650a25b6f1ed719c136c';
+  static const String usersCollection = '650c67914f9943b664a2';
+  static const String tweetsCollection = '650c679ad5f3b00fd551';
+  static const String notificationsCollection = '650c67cc3ddfa5e4639c';
 
   static const String imagesBucket = '63cbdab48cdbccb6b34e';
 
